cbdfd0d9f7b7ca250b814280439e36dea698c3a4
H4zlq <haziqnaim72@gmail.com>
2021-06-10 11:13:02 +0800

    Added signature

--- sign/signature.js
+++ sign/signature.js
@@ -1,8 +1,29 @@
+const CryptoJS = require("crypto-js");
+
 const d = new Date();
 const time = d.getTime();
 
 const env = {
+    app_key: '101736',
+    path: '/auth/token/create',
     timest: time
 }
 
-module.exports = { env };
+// var concatString = string.app_key.concat(string.timest, string.sign_method);
+
+exports.encryptMessage = (path) =>
+    new Promise((resolve, reject) => {
+    const encryptMessage = CryptoJS.HmacSHA256(path, env.app_key).toString(
+        CryptoJS.enc.Hex,
+    );
+
+    let sign = encryptMessage.toUpperCase();
+
+    return resolve(sign);
+});
+
+let signature = this.encryptMessage(env.path);
+
+console.log(signature);
+
+module.exports = { env, signature };
