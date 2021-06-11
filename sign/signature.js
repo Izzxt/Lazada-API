8441210f58682b541a597768b8ff8f186fc452f5
H4zlq <haziqnaim72@gmail.com>
2021-06-11 11:36:21 +0800

    Added app secret

--- sign/signature.js
+++ sign/signature.js
@@ -5,24 +5,31 @@ const time = d.getTime();
 
 const env = {
     app_key: '101736',
+    app_secret: '2bSZFKTfwmIaZd56dbrnqVLB1JnEBr1n',
     path: '/auth/token/create',
     timest: time
 }
 
-// var concatString = string.app_key.concat(string.timest, string.sign_method);
+const string = [
+    `app_key${env.app_key}`,
+    `timestamp${env.timest}`,
+    'sign_methodsha256'
+]
+
+var concatString = string[0].concat(string[1], string[2]);
 
 exports.encryptMessage = (path) =>
     new Promise((resolve, reject) => {
-    const encryptMessage = CryptoJS.HmacSHA256(path, env.app_key).toString(
-        CryptoJS.enc.Hex,
-    );
+        const encryptMessage = CryptoJS.HmacSHA256(path, env.app_key).toString(
+            CryptoJS.enc.Hex,
+        );
 
-    let sign = encryptMessage.toUpperCase();
+        let sign = encryptMessage.toUpperCase();
 
-    return resolve(sign);
-});
+        return resolve(sign);
+    });
 
-let signature = this.encryptMessage(env.path);
+let signature = this.encryptMessage(`${env.path}${concatString}`);
 
 console.log(signature);
 
